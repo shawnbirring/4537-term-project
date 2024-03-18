3bc4bc24d9d4866c6a0675e69144e06f37e12d78
shawnbirring <shawnbirring21@gmail.com>
2024-03-18 15:35:23 -0700

    switched to bcrypt

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 const { PrismaClient } = require('@prisma/client');
 
@@ -21,9 +22,32 @@ const validateLoginInput = [
     body('password').notEmpty(),
 ]
 
-app.get("/check", (_, res) => {
-    res.status(200).json({ message: "API is up and running on!!!" });
-});
+const JWT_KEY = process.env.TOKEN
+
+const JWTMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_KEY);
+        const user = await prisma.user.findUnique({ where: { id: decoded.id } });
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while authenticating user' });
+    }
+}
+
+const createToken = (user) => {
+    return jwt.sign({ id: user.id }, JWT_KEY, { expiresIn: '1h' });
+}
 
 app.post('/register', validateRegisterInput, async (req, res) => {
     const errors = validationResult(req);
@@ -34,20 +58,21 @@ app.post('/register', validateRegisterInput, async (req, res) => {
     const { email, password } = req.body;
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        await prisma.user.create({
+        const newUser = await prisma.user.create({
             data: {
                 email,
                 password: hashedPassword
             }
         });
-        res.status(201).json({ message: `User created successfully: ${email}` });
+        const token = createToken(newUser);
+        res.status(201).json({ message: "User created successfully", token });
     } catch (error) {
-        res.status(500).json({ message: `An error occurred while registering user: ${email}` });
+        res.status(500).json({ message: `An error occurred while creating user: ${email}` });
     }
 });
 
 app.post("/login", validateLoginInput, async (req, res) => {
-    const errors = validateLoginInput(req);
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
@@ -58,60 +83,45 @@ app.post("/login", validateLoginInput, async (req, res) => {
             return res.status(404).json({ error: "User Not found" })
         }
 
-        const passwordMatch = bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.status(401).json({ error: "Invalid email or password" });
         }
-        // req.session.userID = user.id;
-        res.status(200).json({ message: "Login Successful" });
+        const token = createToken(user);
+        res.status(200).json({ message: "Login Successful", token });
     } catch (error) {
-        res.status(500).json({ error: "An error occurred while loggin in" });
+        res.status(500).json({ error: "An error occurred while logging in" });
     }
 })
 
-// app.get("/admin", async (req, res) => {
-//     try {
-//         const isAdmin = await prisma.user.findFirst({
-//             where: {
-//                 id: req.session.userID,
-//                 isAdmin: true
-//             }
-//         });
-//         if (!isAdmin) {
-//             return res.status(403).json({ error: 'Forbidden' });
-//         }
-//         res.status(200).json({ message: 'Admin page', apiCalls: isAdmin.apiCalls });
-//     } catch (error) {
-//         res.status(500).json({ error: 'An error occurred while accessing admin page' });
-//     }
-// })
+app.get("/admin", JWTMiddleware, async (req, res) => {
+    try {
+        const isAdmin = await prisma.user.findFirst({
+            where: {
+                id: req.user.id,
+                isAdmin: true
+            }
+        });
+        if (!isAdmin) {
+            return res.status(403).json({ error: 'Forbidden' });
+        }
+        res.status(200).json({ message: 'Admin page', apiCalls: isAdmin.apiCalls });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while accessing admin page' });
+    }
+});
 
-// app.get('/user', async (req, res) => {
-//     try {
-//         const user = await prisma.user.findUnique({
-//             where: { id: req.session.userId }
-//         });
-//         res.status(200).json({ message: 'User landing page', apiCalls: user.apiCalls });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'An error occurred while accessing user page' });
-//     }
-// });
-
-// app.get('/logout', async (req, res) => {
-//     try {
-//         req.session.destroy((err) => {
-//             if (err) {
-//                 return res.status(500).json({ error: 'An error occurred while logging out' });
-//             }
-//             res.clearCookie('connect.sid');
-//             res.status(200).json({ message: 'Logout successful' });
-//         });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'An error occurred while logging out' });
-//     }
-// });
+app.get('/user', JWTMiddleware, async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: req.user.id }
+        });
+        res.status(200).json({ message: 'User landing page', apiCalls: user.apiCalls });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while accessing user page' });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
