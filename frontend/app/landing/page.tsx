98213fbc7f3abcdcc634c9f691170eac29a61b24
AlexSichitiu <asichitiu@gmail.com>
2024-03-20 06:08:35 -0700

    Added user landing page.

--- frontend/app/landing/page.tsx
+++ frontend/app/landing/page.tsx
@@ -1,7 +1,46 @@
+"use client"
+import { toLogin } from "../lib/actions";
+import { useEffect, useState } from "react"
+
 export default function Page() {
-    
+    const [userData, setUserData] = useState<any>(null)
+
+    const token = localStorage.getItem("token")
+    if (!token) {
+        toLogin()
+        return
+    }
+
+    if (!userData) { loadUserData(token).then((res) => {setUserData(res)}) }
+
+    useEffect(() => {
+        if (userData && userData.error) {
+            toLogin()
+        }
+    },
+    [userData])
 
     return (
-        <h1>Landing Page</h1>
+        <>
+            <h1>Landing Page</h1>
+            {userData && <div>Number of API Calls Remaining: {userData.apiCalls}</div>}
+        </>
     )
+}
+
+async function loadUserData(token: string) {
+    try {
+        const response = await fetch('https://4537-term-project-backend.vercel.app/user', {
+            method: 'GET',
+            headers: {
+                'Authorization': token
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+        return data
+    } catch (error) {
+        console.error('Error:', error);
+        return null
+    }
 }
--- frontend/app/lib/actions.ts
+++ frontend/app/lib/actions.ts
@@ -4,3 +4,7 @@ import { redirect } from "next/navigation";
 export async function redirectClient(path: string) {
     redirect(path)
 }
+
+export async function toLogin() {
+    redirect("/temp")
+}
