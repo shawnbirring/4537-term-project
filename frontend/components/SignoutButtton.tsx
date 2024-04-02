e81403ab8b5430bb477fe2061e176eaa5524aed6
AlexSichitiu <asichitiu@gmail.com>
2024-04-02 15:23:16 -0700

    Created signout button that logs user out.

---
+++ frontend/components/SignoutButtton.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { useRouter } from "next/navigation"
+
+export default function SignoutButton() {
+    const router = useRouter()
+
+    async function logout() {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/logout`, {
+            method: "GET",
+            credentials: 'include'
+        })
+        router.push('/')
+    }
+
+    return (
+        <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={logout}>Sign Out</button>
+    )
+}
+
