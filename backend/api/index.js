0091f60cd9d282d102cb821d7d7fa390982d3c98
AlexSichitiu <asichitiu@gmail.com>
2024-04-03 22:19:26 -0700

    css

--- backend/api/index.js
+++ backend/api/index.js
@@ -363,7 +363,8 @@ try {
   app.use(
     "/api-docs",
     swaggerUI.serve,
-    swaggerUI.setup(swaggerDoc, {customCssUrl: CSS_URL})) 
+    swaggerUI.setup(swaggerDoc, {customCssUrl: CSS_URL, customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }'})) 
   } catch (e) {
     console.log(e)
   }
